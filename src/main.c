e15c9b4e1794cb754c7676ed9ffa696363ca95e1
execute233 <1647643661@qq.com>
2026-09-07 14:17:58 +0800

    feat: assemble main and add real guangzhou metro data files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
+#include <locale.h>
 #include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include "ui/ui.h"
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, char *argv[]) {
+#ifdef _WIN32
+    SetConsoleOutputCP(65001);   /* 控制台输出 UTF-8 */
+    SetConsoleCP(65001);         /* 控制台输入 UTF-8 */
+#endif
+    setlocale(LC_ALL, ".UTF-8");
+
+    const char *data_dir = (argc > 1) ? argv[1] : "data";
+    return ui_main_loop(data_dir) == 0 ? 0 : 1;
 }
